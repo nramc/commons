add0e8433672508ef811c969b179c69ac0c6a0ea
Ramachandran Nellaiyappan <ramachandran.nellaiyappan@1und1.de>
2024-03-22 13:10:33 +0100

    [chore] initial commit

--- geojson/src/main/java/com/github/nramc/commons/Main.java
+++
@@ -1,7 +0,0 @@
-package com.github.nramc.commons;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ geojson/src/main/java/com/github/nramc/commons/geojson/main/MainApplication.java
@@ -0,0 +1,7 @@
+package com.github.nramc.commons.geojson.main;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world..!");
+    }
+}
